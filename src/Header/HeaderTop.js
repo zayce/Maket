1e2f81355b1c1b6532c351dc782a048a76865bfc
zayce <zakirabdurrahman2@gmail.com>
2024-10-29 22:13:14 +0400

    The first part of the work is to assemble the profile header

---
+++ src/Header/HeaderTop.js
@@ -0,0 +1,17 @@
+export const HeaderTop = () => {
+  return (
+    <>
+      <div className="HeaderT">
+        <div className="Header-Adress">
+          Адресс:<span className="Adress">г.Москва, ул.Примерная д.1</span>{" "}
+        </div>
+        <div className="Header-mail">
+          Почта:<span className="mail">info@mail.ru</span>
+        </div>
+        <div className="Header-Number">
+          Телефон:<span className="Nubmer">+7(55)123-45-67</span>
+        </div>
+      </div>
+    </>
+  );
+};
